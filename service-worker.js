c3ec103ba609aa8613459ec4246f6c163d70125d
DX06-2019 <chatura.rs@hotmail.com>
2019-03-17 01:34:37 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/DX06/precache-manifest.80ccdc6a5cc3d0f733baf18394945da1.js"
+  "/DX06/precache-manifest.e66d6bae665101eb6aaec614a06e6e1c.js"
 );
 
 workbox.clientsClaim();
